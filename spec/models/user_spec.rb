abcf2870c81d6cc08d825989ea4d1fd8628b0b7f
mar bautista <marilirulita@gmail.com>
2022-12-05 13:38:13 -0800

    add user model and basic test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "create a new user" do
+    it "ask for a username" do
+      user = User.create(:username => nil)
+      expect(user).to_not be_valid
+    end
+    
+    it "save username data" do
+      user = User.create(:username => "Mar")
+      expect(user.username).to eq('Mar')
+      expect(user.username).not_to eq('Dany')
+    end
+  end
+end
