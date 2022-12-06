bfa8bcc96d88287656c76dd0ea953e964d7848ec
mar bautista <marilirulita@gmail.com>
2022-12-05 16:05:48 -0800

    add search controller with routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :searches, only: %i[ index show new create destroy ]
   resources :users, only: %i[ index show new create destroy ]
 
   # Defines the root path route ("/")
